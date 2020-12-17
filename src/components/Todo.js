8dd1da1a181d246c7ad63e7203bd1511b0bef005
Grzegorz Kurzajczyk <grzegorz.kurzajczyk@gmail.com>
2020-12-17 19:55:43 +0100

    bugfixing

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -2,6 +2,10 @@ import React, { Component } from "react";
 
 export default class Todo extends Component {
   render() {
-    return <div></div>;
+    return (
+      <div>
+        <h1>TODO</h1>
+      </div>
+    );
   }
 }
