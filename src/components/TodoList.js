8b6392722b644aa1c5c4e157563552368fec73e7
Grzegorz Kurzajczyk <grzegorz.kurzajczyk@gmail.com>
2020-12-17 19:53:37 +0100

    no logic, but all components are in place

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,7 +1,14 @@
 import React, { Component } from "react";
+import NewTodoForm from "./NewTodoForm";
+import Todo from "./Todo";
 
 export default class TodoList extends Component {
   render() {
-    return <div></div>;
+    return (
+      <div>
+        <NewTodoForm />
+        <Todo />
+      </div>
+    );
   }
 }
