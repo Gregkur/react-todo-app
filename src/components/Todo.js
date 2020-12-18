196401d6973dbe26ee53516b307ad31fc10db2e1
Grzegorz Kurzajczyk <grzegorz.kurzajczyk@gmail.com>
2020-12-18 12:36:37 +0100

    added editing toggle switch

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -3,18 +3,38 @@ import React, { Component } from "react";
 export default class Todo extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      isEditing: false,
+    };
     this.handleDelete = this.handleDelete.bind(this);
+    this.toggleForm = this.toggleForm.bind(this);
   }
   handleDelete() {
     this.props.deleteTodo(this.props.id);
   }
+  toggleForm() {
+    this.setState({ isEditing: !this.state.isEditing });
+  }
   render() {
-    return (
-      <div>
-        <button>Edit</button>
-        <button onClick={this.handleDelete}>X</button>
-        <li>{this.props.task}</li>
-      </div>
-    );
+    let result;
+    if (this.state.isEditing) {
+      result = (
+        <div>
+          <form>
+            <input type="text" />
+            <button>Save</button>
+          </form>
+        </div>
+      );
+    } else {
+      result = (
+        <div>
+          <button onClick={this.toggleForm}>Edit</button>
+          <button onClick={this.handleDelete}>X</button>
+          <li>{this.props.task}</li>
+        </div>
+      );
+    }
+    return result;
   }
 }
